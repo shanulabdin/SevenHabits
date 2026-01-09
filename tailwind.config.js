a9a00f696482bda2c7ddb3c6bc26be0e4691be05
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-09 11:33:18 +0500

    fic error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+    content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#222222",
+        orange: '#FF6D1F',
+        light: '#F5E7C6',
+        lighter: '#FAF3E1',
+        text: '#FFFFFF',
+        dark: '#151515',
+      },
+    },
   },
   plugins: [],
 }
