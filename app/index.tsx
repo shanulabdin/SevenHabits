0dfc77960e268247c565a87a850275943d08553f
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-10 08:15:00 +0500

    disable headers

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      className="flex-1 justify-center items-center"
+      className="flex-1 justify-center items-center bg-background"
     >
-      <Text className="text-5xl text-text">Welcome!</Text>
+      <Text className="text-5xl text-text">SevenHabits!</Text>
     </View>
   );
 }
