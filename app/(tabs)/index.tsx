4f6e804acffb61af4a3959127cb755b34241fc55
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-14 15:14:59 +0500

    made the habit check

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,37 @@
-import { Text, View } from "react-native";
+import { colors } from '@/constants/colors';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import React, { useState } from 'react';
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <View
-      className="flex-1 justify-center items-center bg-colors-dark"
+      className="flex-1 items-center bg-colors-dark p-4 "
     >
-      <Text className="text-5xl text-colors-light">SevenHabits!</Text>
+      <View className="
+        flex-row 
+        mt-8 
+        space-x-4 
+        bg-colors-background 
+        w-[100%] 
+        p-4 
+        rounded-xl
+        items-center
+        justify-between
+        space-between
+        ">
+
+        <Text className="text-colors-light font-normal text-2xl">Code</Text>
+        <Pressable
+          onPress={() => {
+            setIsChecked(!isChecked);
+          }}
+        >
+          <Ionicons name={isChecked ? "checkbox" : "square-outline"} size={26} color={colors.light} />
+        </Pressable>
+      </View>
     </View>
   );
 }
