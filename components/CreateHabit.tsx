ff78763334cd83d6350611c365ee42d0e4e21746
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-15 20:29:13 +0500

    create habit color light

--- components/CreateHabit.tsx
+++ components/CreateHabit.tsx
@@ -13,11 +13,12 @@ export default function CreateHabit({ newHabitTitle, setNewHabitTitle, createHab
   return (
     <View className="w-full flex-row items-center justify-between bg-colors-background rounded-xl p-2 " >
       <TextInput
-        className={`
+        className="
           flex-1
           font-normal
           text-2xl
-        `}
+          text-colors-light
+        "
         placeholderTextColor={colors.orange}
         placeholder="Create Habit"
         onChangeText={setNewHabitTitle}
