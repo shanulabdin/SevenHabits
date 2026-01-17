ee5da7f36548ad68d357fe8271649d1868e0bb89
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-17 16:13:45 +0500

    making the new design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import "./globals.css";
- 
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // or splash screen
+  }
   return <Stack screenOptions={{ headerShown: false }} />;
 }
--- components/HabitCard.tsx
+++ components/HabitCard.tsx
@@ -1,7 +1,7 @@
 import { colors } from "@/constants/colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 type HabitCardProps = {
   title: string;
@@ -16,34 +16,42 @@ export default function HabitCard({ title, checked, markComplete, onLongPress }:
         flex-row 
         bg-colors-background 
         border-b-[1px]
-        border-b-colors-light/20 
         w-full
         p-4 
-        rounded-xl
+        rounded-tr-2xl
+        rounded-bl-2xl
         items-center
         justify-between
+        mb-3
       "
 
-        onLongPress={onLongPress}
-      >
-      <Text className={`
-        text-colors-light 
-        font-normal 
-        text-2xl 
-        max-w-[80%]
-        ${checked ? 'line-through opacity-50' : ''} 
+      onLongPress={onLongPress}
+    >
+      <View className="flex-row items-baseline min-w-[48px]">
+        <Text style={{fontFamily: "Poppins_600SemiBold"}} className={`
+        text-colors-text text-sm
+        `}>130</Text>
+        <Ionicons name="flame" color={colors.orange} size={20} style={{transform : [{translateY: 1}]}}></Ionicons>
+      </View>
+
+      <Text style={{fontFamily: "Poppins_600SemiBold"}} className={`
+        text-colors-text 
+        font-semibold 
+        text-xl 
+        text-center
       `}>{title}</Text>
+
 
       <Pressable
         onPress={markComplete}
         hitSlop={12}
 
-        className={`w-10 h-10 rounded-full items-center justify-center ${checked ? "bg-colors-orange" : "bg-colors-orange/30"
+        className={`w-8 h-8 rounded-full items-center justify-center ${checked ? "bg-colors-orange" : "bg-colors-orange/30"
           }`}
       >
-        <Ionicons name="checkmark-sharp" size={28} color={colors.dark} />
+        <Ionicons name="checkmark-sharp" size={20} color={colors.dark} />
       </Pressable>
-      
+
     </Pressable>
   );
 }
