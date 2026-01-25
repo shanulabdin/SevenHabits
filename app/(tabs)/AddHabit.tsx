00d45a006fb5d1e44f64c88d8f96e426776d2d43
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-25 16:11:54 +0500

    Restyle AddHabit screen

--- app/(tabs)/AddHabit.tsx
+++ app/(tabs)/AddHabit.tsx
@@ -1,8 +1,9 @@
 import CreateHabit from "@/components/CreateHabit";
 import Heading from "@/components/Heading";
+import { colors } from "@/constants/colors";
 import { router } from "expo-router";
 import { useState } from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function AddHabit() {
   const [newHabitTitle, setNewHabitTitle] = useState("");
@@ -21,18 +22,13 @@ export default function AddHabit() {
   }
 
   return (
-    <View className="
-      flex-1 
-      items-center 
-      bg-black
-      p-3 pt-20 
-      w-full 
-    ">
+    <View style={[styles.container, { backgroundColor: colors.dark }]}>
       <Heading
         title="Add Habit"
         iconTitle="Save"
         icon="checkmark"
-        onIconPress={() => submit(newHabitTitle)} />
+        onIconPress={() => submit(newHabitTitle)}
+      />
       <CreateHabit
         newHabitTitle={newHabitTitle}
         setNewHabitTitle={setNewHabitTitle}
@@ -41,3 +37,13 @@ export default function AddHabit() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 12,
+    paddingTop: 80,
+    width: "100%",
+  },
+});
